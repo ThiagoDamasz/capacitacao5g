49cf0260a6bc833d35d8596da22595dd22d35a31
Thiago Damas <thiago.damas@ges.inatel.br>
2024-05-05 19:34:00 -0300

    criado o exercicio soma

---
+++ Exercicios_aula1/soma/src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int soma = 0;
+
+        for (int i = 1; i <= 15; i++) {
+            soma = i + soma;
+        }
+
+        System.out.println("A soma dos e igual a: " + soma);
+    }
+}
