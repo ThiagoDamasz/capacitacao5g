130df72a579da49e98f8fc987757c8d003c2c661
Thiago Damas <thiago.damas@ges.inatel.br>
2024-05-05 19:11:53 -0300

    criando exercicio diasDaSemana

---
+++ Exercicios_aula1/diasDaSemana/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o dia: ");
+        int dia = sc.nextInt();
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+        }
+    }
+}
